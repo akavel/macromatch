c494cd6b2e141323895417da77fb101517583478
Mateusz Czapliński <czapkofan@gmail.com>
2019-10-03 09:30:26 +0200

    try adding first test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,14 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
+import macros
 
 import macromatch
-test "can add":
-  check add(5, 5) == 10
+
+static:
+  test "simple ident":
+    let
+      t1 = ident("foobar")
+    check t1 =~ Ident(_)
+    check t1 =~ Ident("foobar")
+    check t1 !~ Empty(_)
+    check t1 !~ StmtList(_)
+
